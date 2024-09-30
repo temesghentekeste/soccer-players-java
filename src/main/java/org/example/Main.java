9d8fc96dfb9842f7ea1cb2f1d3a2d2d5d32ff66e
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2024-09-30 16:21:33 +0300

    Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,39 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        SoccerPlayer player1 = new SoccerPlayer("Erling Haaland", 23, "Manchester City", 36);
+        SoccerPlayer player2 = new SoccerPlayer("Mohamed Salah", 31, "Liverpool", 25);
+        SoccerPlayer player3 = new SoccerPlayer("Harry Kane", 29, "Tottenham Hotspur", 20);
+        SoccerPlayer player4 = new SoccerPlayer("Kevin De Bruyne", 32, "Manchester City", 15);
+        SoccerPlayer player5 = new SoccerPlayer("Bukayo Saka", 23, "Arsenal", 5);
+        SoccerPlayer player6 = new SoccerPlayer("Martin Odegard", 24, "Arsenal", 5);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // testing
+        System.out.println(player1);
+        System.out.println(player2);
+
+        // equality
+        if(player4.equals(player5)) {
+            System.out.println("Equal");
+            System.out.println(player4);
+            System.out.println(player5);
+        } else {
+            System.out.println("Not equal");
+            System.out.println(player4);
+            System.out.println(player5);
         }
+
+        System.out.println();
+        // equality
+        if(player6.equals(player5)) {
+            System.out.println("Equal");
+            System.out.println(player6);
+            System.out.println(player5);
+        } else {
+            System.out.println("Not equal");
+            System.out.println(player6);
+            System.out.println(player5);
+        }
+
     }
 }
